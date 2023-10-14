32b1716090e60fb03143c04894e83dbad35a15bb
Robert Falkbäck Rovenko <robert.ntisthlm@gmail.com>
2023-10-14 10:55:43 +0200

    connecting my project to github

---
+++ sidehustlers/sidehustlersApp.swift
@@ -0,0 +1,17 @@
+//
+//  sidehustlersApp.swift
+//  sidehustlers
+//
+//  Created by Robert Falkbäck on 2023-10-14.
+//
+
+import SwiftUI
+
+@main
+struct sidehustlersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
