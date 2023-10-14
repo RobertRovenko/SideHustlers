1d4688ea1a6e65b7eb5729d3308ebf3be47f949c
Robert Falkbäck Rovenko <robert.ntisthlm@gmail.com>
2023-10-14 18:42:23 +0200

    Textfields cant be empty in PostView and designed some on the homeview

--- sidehustlers/Views/HomeView.swift
+++ sidehustlers/Views/HomeView.swift
@@ -15,10 +15,24 @@ struct HomeView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Text("Home Screen Content")
+                Image("SHIcon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                
+                HStack {
+                           Text("Available Tasks")
+                               .frame(maxWidth: .infinity, alignment: .leading)
+                           Spacer()
+                       }
+                       .padding()
+                
                 List(choreViewModel.chores, id: \.id) { chore in
-                    Text(chore.title)
-                }
+                        VStack(alignment: .leading) {
+                            Text(chore.title)
+                            Text("Reward: \(chore.reward) kr")
+                        }
+                    }
 
                 Spacer()
             }
--- sidehustlers/Views/PostView.swift
+++ sidehustlers/Views/PostView.swift
@@ -14,7 +14,11 @@ struct PostView: View {
     @State private var chore = Chore(title: "", description: "", reward: 0)
     @EnvironmentObject var choreViewModel: ChoreViewModel
     @State private var showAlert = false
-    
+
+    var fieldsAreEmpty: Bool {
+        return chore.title.isEmpty || chore.description.isEmpty || chore.reward == 0
+    }
+
     var body: some View {
         NavigationView {
             Form {
@@ -22,40 +26,45 @@ struct PostView: View {
                     TextField("Title", text: $chore.title)
                     TextField("Description", text: $chore.description)
                     TextField("Reward", text: Binding(
-                                           get: {
-                                               "\(chore.reward)"
-                                           },
-                                           set: {
-                                               if let newValue = Int($0) {
-                                                   chore.reward = newValue
-                                               }
-                                           }
-                                       ))
+                        get: {
+                            "\(chore.reward)"
+                        },
+                        set: {
+                            if let newValue = Int($0) {
+                                chore.reward = newValue
+                            }
+                        }
+                    ))
                 }
-                    Section {
-                        Button(action: {
+                Section {
+                    Button(action: {
+                        if !fieldsAreEmpty {
                             choreViewModel.addChore(chore: chore)
                             showAlert = true
                             clearFields()
-                        }) {
-                            Text("Post Chore")
+                        } else {
+                            //Show an error message if fields are empty.
                         }
+                    }) {
+                        Text("Post Chore")
                     }
                 }
             }
-            .alert(isPresented: $showAlert) {
-                Alert(
-                    title: Text("Success"),
-                    message: Text("Chore posted successfully."),
-                    dismissButton: .default(Text("OK")) {
-                               
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("Success"),
+                message: Text("Chore posted successfully."),
+                dismissButton: .default(Text("OK")) {
+                   
                 }
             )
         }
     }
+
     func clearFields() {
-           chore.title = ""
-           chore.description = ""
-            chore.reward = 0
-       }
+        chore.title = ""
+        chore.description = ""
+        chore.reward = 0
+    }
 }
